c4f2f54b9144f3f8e417a0c5c9473246dbfdd768
AgustinS2 <agussegovia321@gmail.com>
2023-10-18 15:24:49 -0300

    feat: initial structure

---
+++ app/src/main/java/com/example/parcial_1_am_acn4b_segovia_agustin_forcada_agustin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial_1_am_acn4b_segovia_agustin_forcada_agustin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
