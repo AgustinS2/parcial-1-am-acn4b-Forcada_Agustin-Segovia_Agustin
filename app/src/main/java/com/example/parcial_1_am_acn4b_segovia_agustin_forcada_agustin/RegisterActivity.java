a2a321a4ed81c51204e8401ae8c5d7f9140b6363
AgustinS2 <agussegovia321@gmail.com>
2023-10-25 20:09:24 -0300

    fixed: methods

--- app/src/main/java/com/example/parcial_1_am_acn4b_segovia_agustin_forcada_agustin/RegisterActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4b_segovia_agustin_forcada_agustin/RegisterActivity.java
@@ -1,17 +1,37 @@
 package com.example.parcial_1_am_acn4b_segovia_agustin_forcada_agustin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private Button registerButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_register);
 
+        usernameEditText = findViewById(R.id.editTextUsername);
+        registerButton = findViewById(R.id.buttonRegister);
 
-
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                if (!username.isEmpty()) {
+                    // Aquí puedes realizar alguna dinámica visual, por ejemplo, mostrar un mensaje de bienvenida
+                    Toast.makeText(RegisterActivity.this, "¡Bienvenido, " + username + "!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Por favor, ingresa un nombre de usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
