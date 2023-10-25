e33976b291cd13f69962e89e1a7fb05c1156dcd5
aforcada <Forcadaagustin@gmail.com>
2023-10-25 15:18:40 -0300

    feat::creacion de vista de registro

---
+++ app/src/main/java/com/example/parcial_1_am_acn4b_segovia_agustin_forcada_agustin/RegisterActivity.java
@@ -0,0 +1,6 @@
+package com.example.parcial_1_am_acn4b_segovia_agustin_forcada_agustin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+}
