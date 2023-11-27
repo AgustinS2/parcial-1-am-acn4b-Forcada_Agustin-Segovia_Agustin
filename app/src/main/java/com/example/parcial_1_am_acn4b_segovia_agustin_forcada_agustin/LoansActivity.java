905d0d1ddbc13937ba4d30c3910f4442e3a99700
AgustinS2 <agussegovia321@gmail.com>
2023-11-27 20:32:41 -0300

    feat: add view of loans

---
+++ app/src/main/java/com/example/parcial_1_am_acn4b_segovia_agustin_forcada_agustin/LoansActivity.java
@@ -0,0 +1,18 @@
+package com.example.parcial_1_am_acn4b_segovia_agustin_forcada_agustin;
+
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoansActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loans);
+
+    }
+}
